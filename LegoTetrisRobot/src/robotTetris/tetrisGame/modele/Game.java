d64cbcfbf243e9aedcf6c95d7a4b26c4e874a271
Gaëtan <lebarbe@ecole.ensicaen.fr>
2013-12-08 16:58:00 +0100

    Suppression champ inutiles dans Game

--- LegoTetrisRobot/src/robotTetris/tetrisGame/modele/Game.java
+++ LegoTetrisRobot/src/robotTetris/tetrisGame/modele/Game.java
@@ -1,15 +1,8 @@
 package robotTetris.tetrisGame.modele;
 
-import java.util.Vector;
 
 public class Game implements TetrisListener
 {
-
-	public Vector myBoard;
-	public Vector myBoard;
-	public Vector myBoard;
-	public Vector myPlayer;
-	public Vector myTetrominoGenerator;
 
 	public void tick()
 	{
